03b22b57dfd5e90d7c93d8db047529ab9c15ac03
tuyengheh <pdklh19@gmail.com>
2026-03-27 23:56:38 +0700

    Remove lag functionality from Script.lua

--- Script.lua
+++ Script.lua
@@ -6,25 +6,3 @@ RunService.RenderStepped:Connect(function()
         task.wait(0.2)
     end
 end)
-
-
-local lag = false
-
-game:GetService("UserInputService").InputBegan:Connect(function(i,g)
-    if not g and i.KeyCode == Enum.KeyCode.L then
-        lag = not lag
-        print("Lag:", lag)
-    end
-end)
-
-task.spawn(function()
-    while true do
-        if lag then
-            -- spam tính toán nặng
-            for i = 1, 500000 do
-                local a = math.sqrt(i)
-            end
-        end
-        task.wait()
-    end
-end)
